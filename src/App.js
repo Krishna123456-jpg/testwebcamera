f1b052e0b6e726a28845ecb4e17194adc256d588
Krishna123456-jpg <80893584+Krishna123456-jpg@users.noreply.github.com>
2023-08-04 17:25:21 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const src =
+  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
+
+const Video = () => {
+  return (
+    <video controls width="100%">
+      <source src={src} type="video/mp4" />
+      Sorry, your browser doesn't support embedded videos.
+    </video>
+  );
+};
+
+export default Video;
+
+
